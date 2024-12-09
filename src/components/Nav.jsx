2703c0865e861dbe302c7271aa789b5aa0a0e705
Debimpeee <adebimpefarinde@gmail.com>
2024-12-09 15:56:04 +0100

    added dropdown menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {headerLogo} from '../assets/images'
-import {hamburger} from "../assets/icons"
+import {hamburger, close} from "../assets/icons"
 import { navLinks } from '../constants'
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+  
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
@@ -13,6 +19,8 @@ const Nav = () => {
              width={130}
              height={29}/>
         </a>
+
+        {/* Desktop navlinks */}
         <ul className="flex-1 flex justify-center items center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
@@ -24,105 +32,46 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className='hidden max-lg:block'>
+
+        {/* Mobile menu buttons */}
+        <div className='hidden max-lg:block cursor-pointer z-30' onClick={toggleMenu}>
           <img 
-           src={hamburger}
-           alt="hamburger"
+           src={menuOpen ? close : hamburger}
+           alt={menuOpen ? "close menu" : "hamburger menu"}
            width={25}
-           height={25} />
+           height={25}/>
         </div>
       </nav>
+
+      {/* Mobile navlinks */}
+      {menuOpen && (
+          <ul className="fixed top-0 left-0 flex flex-col items-center justify-center w-full h-screen bg-white gap-10 z-10 ${menuOpen ? 'translate-x-0' : '-translate-x-full}">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                href={item.href}
+                className="font-montserrat text-slate-gray text-lg leading-normal"
+                onClick={toggleMenu}>
+                  {item.label}
+                </a>
+              </li>
+            ))}
+        </ul>
+      )}
     </header>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   )
 }
 
 export default Nav
+
+
+
+
+
+
+
+
+
+
+
+
